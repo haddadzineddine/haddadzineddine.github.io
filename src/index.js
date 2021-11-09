9497698aa467bf6114d594edf49334a3a9f2589a
zineddine <hz_haddad@esi.dz>
2021-11-09 13:47:20 +0100

    v1.1

--- src/index.js
+++ src/index.js
@@ -5,16 +5,12 @@ import './assets/images/haddad.png';
 import './assets/images/php-banner.png';
 import './assets/images/covid.png';
 import './assets/images/school.png';
+import './assets/images/dev.png';
 import './assets/images/Haddad_Zineddine_Resume.pdf';
 
 import scroll from './modules/scroll';
 import { toggleTheme, addBodyClass, addBtnClass } from './modules/toggleTheme';
 import displayList from './modules/displayList';
-
-
-
-
-
 
 const btnHamburger = document.querySelector('.fa-bars');
 const btnTheme = document.querySelector('.fa-moon');
